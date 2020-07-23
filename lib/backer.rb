dfddf43c8c081ebcd16ed859532686fe71e3e1b2
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-23 01:49:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,8 @@
 class Backer
-  attr_accessor :backed_projects
-  attr_reader :name 
+  attr_reader :name, :backed_project = []
   
   def initialize(name)
-    @name = name 
+    @name = name
   end
   
 end 
